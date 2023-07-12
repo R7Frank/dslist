0d3500aa98d87f63d4ccc91e2fd48ffc36095ef9
R7Frank <r7frank@hotmail.com>
2023-07-12 03:00:51 -0300

    Projeto criado

---
+++ dslist/src/main/java/com/r7frank/dslist/DslistApplication.java
@@ -0,0 +1,13 @@
+package com.r7frank.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistApplication.class, args);
+	}
+
+}
